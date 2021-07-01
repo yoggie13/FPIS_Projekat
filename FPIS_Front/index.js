30a372cce44ead1eb35bd4dae8f7ad2346f5b0f4
yoggie13 <ognjennikolic13@gmail.com>
2021-07-01 15:16:45 +0200

    CIta podatke iz searcha

--- FPIS_Front/index.js
+++ FPIS_Front/index.js
@@ -13,7 +13,28 @@ $("#searchBar").submit(function (event) {
         url: "http://localhost:11807/api/Devices/" + searchTerm,
         type: "POST",
         success: function (data) {
-            console.log(data);
+            data.forEach(element => {
+                var div = document.createElement("div");
+                div.id = element["name"];
+                div.className = "results";
+
+                var h2 = document.createElement('h2');
+                h2.innerHTML = element['name'];
+
+                var color = document.createElement('p');
+                color.id = "color";
+                color.innerHTML = element['color'];
+
+                var price = document.createElement('p');
+                price.id = 'price';
+                price.innerHTML = element['price'];
+
+                div.append(h2);
+                div.append(price);
+                div.append(color);
+
+                $("#searchResults").append(div);
+            });
         }
 
     });
